f6aa118b1c9c315f7581caa4b74baf324bfd8d57
Kwaku Appiah-Adu <kwakuappiahadu@gmail.com>
2023-01-03 23:54:38 +0000

    customer service to store customer in db

--- customer/src/main/java/com/microservice/customer/CustomerService.java
+++ customer/src/main/java/com/microservice/customer/CustomerService.java
@@ -3,12 +3,15 @@ package com.microservice.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.fistName())
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
+
+        // store customer in db
+        customerRepository.save(customer);
     }
 }
