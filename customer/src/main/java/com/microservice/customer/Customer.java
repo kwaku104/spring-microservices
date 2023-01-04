acdccd987f2575738210aaf84bc6914421cbf242
Kwaku Appiah-Adu <kwakuappiahadu@gmail.com>
2023-01-04 22:57:28 +0000

    Added some comments

--- customer/src/main/java/com/microservice/customer/Customer.java
+++ customer/src/main/java/com/microservice/customer/Customer.java
@@ -13,8 +13,12 @@ import javax.persistence.GenerationType;
 
 @Builder
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+@AllArgsConstructor // Constructor with all fields as parameters
+// JPA specification requires that all persistent classes (@Entity) have
+// a no-arg contructor, public or protected.
+@NoArgsConstructor  // Constructor with no parameters
+// Entities are POJOs representing data that can be persisted to the database
+// Every instance of an entity represents a row in a database
 @Entity
 public class Customer {
 
@@ -23,6 +27,7 @@ public class Customer {
             name = "customer_id_sequence",
             sequenceName = "customer_id_sequence"
     )
+    // Defines the types of primary key generation strategies
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
             generator = "customer_id_sequence"
