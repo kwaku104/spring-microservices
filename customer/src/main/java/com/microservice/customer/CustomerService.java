6b61151ef9b34143076ab306ac4561d3bf274a99
Kwaku Appiah-Adu <kwakuappiahadu@gmail.com>
2023-01-23 23:15:19 +0000

    customer restTemplate consuming fraud service api to check if customer is a fraudster

--- customer/src/main/java/com/microservice/customer/CustomerService.java
+++ customer/src/main/java/com/microservice/customer/CustomerService.java
@@ -1,14 +1,17 @@
 package com.microservice.customer;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
-    public CustomerService(CustomerRepository customerRepository) {
+    public CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate) {
         this.customerRepository = customerRepository;
+        this.restTemplate = restTemplate;
     }
 
     public void registerCustomer(CustomerRegistrationRequest request) {
@@ -17,8 +20,23 @@ public class CustomerService {
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
+        // todo: check if email is valid
+        // todo: check if email is not taken
 
         // store customer in db
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+
+        // todo: check if fraudster
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject("http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        assert fraudCheckResponse != null;
+        if (fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
+
+        // todo: send notification
     }
 }
