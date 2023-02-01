cd460dcdbb5ea8bcf8a05eaf6f0cfe6248d540aa
Kwaku Appiah-Adu <kwakuappiahadu@gmail.com>
2023-02-01 11:40:10 +0000

    Fixed typo in url, and logging

--- fraud/src/main/java/com/microservice/fraud/FraudController.java
+++ fraud/src/main/java/com/microservice/fraud/FraudController.java
@@ -1,9 +1,11 @@
 package com.microservice.fraud;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/fraud-checker")
+@Slf4j
 public class FraudController {
 
     private FraudCheckService fraudCheckService;
@@ -17,6 +19,7 @@ public class FraudController {
             @PathVariable("customerId") Integer customerID
     ) {
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerID);
+        log.info("fraud check request for customer {}", customerID);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
